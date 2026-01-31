f219006140515184f595f301a7dc77a19d9d8f5e
Tonyhzk <1125258615@qq.com>
2026-01-31 13:43:04 +0800

    删除缓存

--- src/src-tauri/src/target/debug/build/serde_core-1e40bac5d8368a8c/build_script_build-1e40bac5d8368a8c.d
+++
@@ -1,5 +0,0 @@
-Y:\Documents\GitHub\cc-permission\src\src-tauri\target\debug\build\serde_core-1e40bac5d8368a8c\build_script_build-1e40bac5d8368a8c.d: C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\serde_core-1.0.228\build.rs
-
-Y:\Documents\GitHub\cc-permission\src\src-tauri\target\debug\build\serde_core-1e40bac5d8368a8c\build_script_build-1e40bac5d8368a8c.exe: C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\serde_core-1.0.228\build.rs
-
-C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\serde_core-1.0.228\build.rs:
--- src/src-tauri/src/target/debug/deps/unicode_ident-15e12909c4995f30.d
+++
@@ -1,8 +0,0 @@
-Y:\Documents\GitHub\cc-permission\src\src-tauri\target\debug\deps\unicode_ident-15e12909c4995f30.d: C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\lib.rs C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\tables.rs
-
-Y:\Documents\GitHub\cc-permission\src\src-tauri\target\debug\deps\libunicode_ident-15e12909c4995f30.rlib: C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\lib.rs C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\tables.rs
-
-Y:\Documents\GitHub\cc-permission\src\src-tauri\target\debug\deps\libunicode_ident-15e12909c4995f30.rmeta: C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\lib.rs C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\tables.rs
-
-C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\lib.rs:
-C:\Users\jjp\.cargo\registry\src\rsproxy.cn-e3de039b2554c837\unicode-ident-1.0.22\src\tables.rs:
